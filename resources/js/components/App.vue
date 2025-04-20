8f7570afc79fd37aa43708a9f299cb96489f1581
Heric Honkis <hericbranco@gmail.com>
2025-04-20 15:27:40 -0300

    feat - adicionando vue,vite,tailwind

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Welcome to Laravel 12 with Vue 3</h1>
+    </div>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+    export default {
+        mounted() {
+            console.log('App Mounted');
+        }
+    }
+</script>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h2>This is the Home Page</h2>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    </div>
+</template>
