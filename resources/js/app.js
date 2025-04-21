6fe25a4ac198f05ba16aaa567181ea49a7365188
Heric Honkis <hericbranco@gmail.com>
2025-04-21 16:13:36 -0300

    feat - melhorando o layou

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,17 @@
 import { createApp } from 'vue';
 import App from './pages/App.vue';
+import VueApexCharts from "vue3-apexcharts";
 import router from './router/index.js';
+import axios from 'axios';
 
 import Header from './components/Header.vue';
 import SideMenu from './components/SideMenu.vue';
+import HeaderTotais from './components/dashboard/HeaderTotais.vue';
 
 const app = createApp(App).use(router);
-
+app.use(VueApexCharts);
 app.component('app-header', Header);
 app.component('app-sidemenu', SideMenu);
+app.component('app-header-totais', HeaderTotais);
 
 app.mount('#app');
